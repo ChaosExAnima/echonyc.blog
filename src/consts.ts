ddf1476197cf82aee149f59f0021d07b81cafda6
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-01-22 22:16:15 +0100

    start restyling everything!

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'Echo in NYC';
-export const SITE_DESCRIPTION = 'Yelling into the void';
+export const SITE_DESCRIPTION = 'Musing into the void';
