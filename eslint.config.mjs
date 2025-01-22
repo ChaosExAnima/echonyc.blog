8992a8a8946eea940ef54d5fb460dc2a20656b18
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-01-22 16:22:26 +0100

    lint astro files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,14 @@
-import { FlatCompat } from '@eslint/eslintrc';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginAstro from 'eslint-plugin-astro';
 import perfectionist from 'eslint-plugin-perfectionist';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-});
 
 const eslintConfig = [
 	perfectionist.configs['recommended-alphabetical'],
 	...eslintPluginAstro.configs['jsx-a11y-recommended'],
 	eslintConfigPrettier,
+	{
+		ignores: ['.astro/'],
+	},
 ];
 
 export default eslintConfig;
