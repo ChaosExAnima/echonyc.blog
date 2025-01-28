3f5233f75aa7f99665212789061af661c0ae61c2
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-01-29 00:32:10 +0100

    remove Partytown integration and update script loading in BaseLayout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), sitemap(), tailwind(), partytown()],
+	integrations: [mdx(), sitemap(), tailwind()],
 	redirects: {
 		'/my-pronouns': '/pronouns',
 	},
