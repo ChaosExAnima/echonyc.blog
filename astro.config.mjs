42a774df88d5ca474e3ff6c060d89a23a73658ca
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-01-30 18:26:15 +0100

    enhance layout structure and accessibility; add robots.txt generation and update component styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,17 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
-
 import pageInsight from 'astro-page-insight';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        responsiveImages: true,
-    },
-    integrations: [mdx(), sitemap(), tailwind(), pageInsight()],
-    redirects: {
-        '/my-pronouns': '/pronouns',
-    },
-    site: 'https://echonyc.blog',
+	experimental: {
+		responsiveImages: true,
+	},
+	integrations: [mdx(), sitemap(), tailwind(), pageInsight()],
+	redirects: {
+		'/my-pronouns': '/pronouns',
+	},
+	site: 'https://echonyc.blog',
 });
