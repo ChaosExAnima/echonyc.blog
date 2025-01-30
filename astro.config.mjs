8c121e7420396794a0ba0e11d7f2b7636a91f816
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-01-30 18:02:30 +0100

    add astro-pagefind integration and update Header component with search functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,21 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
-
 import pageInsight from 'astro-page-insight';
+import pagefind from 'astro-pagefind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        responsiveImages: true,
-    },
-    integrations: [mdx(), sitemap(), tailwind(), pageInsight()],
-    redirects: {
-        '/my-pronouns': '/pronouns',
-    },
-    site: 'https://echonyc.blog',
+	build: {
+		format: 'file',
+	},
+	experimental: {
+		responsiveImages: true,
+	},
+	integrations: [mdx(), sitemap(), tailwind(), pageInsight(), pagefind()],
+	redirects: {
+		'/my-pronouns': '/pronouns',
+	},
+	site: 'https://echonyc.blog',
 });
