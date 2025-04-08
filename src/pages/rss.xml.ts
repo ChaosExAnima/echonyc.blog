c29b397935352169dc3409164585777f32dcfc8b
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-04-08 20:12:54 +0200

    fix RSS bug and add discovery

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,12 @@
 import type { APIContext } from 'astro';
 
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
 
 import { SITE_DESCRIPTION, SITE_TITLE } from '~/consts';
-import { getPostPath } from '~/lib/posts';
+import { getPostPath, getPosts } from '~/lib/posts';
 
 export async function GET(context: APIContext) {
-	const posts = await getCollection('blog');
+	const posts = await getPosts();
 	return rss({
 		description: SITE_DESCRIPTION,
 		items: posts.map((post) => ({
