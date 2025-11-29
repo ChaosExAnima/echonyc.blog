b849b266e66985508ea15aa0724f5df67b1223a3
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-11-29 22:44:40 +0100

    fix ESLint issues, update Astro to latest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,13 +4,12 @@ import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginAstro from 'eslint-plugin-astro';
 import perfectionist from 'eslint-plugin-perfectionist';
-import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
-const eslintConfig = tseslint.config(
+const eslintConfig = defineConfig(
 	eslint.configs.recommended,
-	tseslint.configs.recommended,
 	perfectionist.configs['recommended-alphabetical'],
-	eslintPluginAstro.configs['jsx-a11y-recommended'],
+	...eslintPluginAstro.configs['jsx-a11y-recommended'],
 	eslintConfigPrettier,
 	{
 		rules: {
